2bfc6d5b8ac1f96a987fef4acef92db7455b8d5c
Cj-bc <cj.bc-sd@outlook.jp>
2020-03-20 16:35:17 +0900

    Add: comment for FaceData

--- FaceDataServer/Type/FaceData.cs
+++ FaceDataServer/Type/FaceData.cs
@@ -10,42 +10,31 @@ namespace Cjbc.FaceDataServer.Type {
     /// reference at: https://github.com/Cj-bc/FDS-protos
     /// </remarks>
     public class FaceData {
-        /// <summary>
-        ///     face x rotation in radian.
-        ///     Should be:
-        ///     <code>
-        ///         Math.Clamp(FaceXRadian, -Math.PI, Math.PI) == FaceXRadian
-        ///     </code>
-        /// </summary>
+        /// <summary>face x rotation in radian.</summary>
+        /// <remarks>This should be range of (-<c>Math.PI</c>, <c>Math.PI</c>)</remarks>
         public readonly float FaceXRadian;
 
-        /// <summary>
-        ///     face y rotation in radian.
-        ///     Should be:
-        ///     <code>
-        ///         Math.Clamp(FaceYRadian, -Math.PI, Math.PI) == FaceYRadian
-        ///     </code>
-        /// </summary>
+        /// <summary>face y rotation in radian.</summary>
+        /// <remarks>This should be range of (-<c>Math.PI</c>, <c>Math.PI</c>)</remarks>
         public readonly float FaceYRadian;
 
-        /// <summary>
-        ///     face z rotation in radian.
-        ///     Should be:
-        ///     <code>
-        ///         Math.Clamp(FaceZRadian, -Math.PI, Math.PI) == FaceZRadian
-        ///     </code>
-        /// </summary>
+        /// <summary>face z rotation in radian.</summary>
+        /// <remarks>This should be range of (-<c>Math.PI</c>, <c>Math.PI</c>)</remarks>
         public readonly float FaceZRadian;
 
         /// <summary>how much percent current mouth height is comparing to the default.</summary>
+        /// <remarks>This should be range of (0, 150)</remarks>
         public readonly int   MouthHeightPercent;
 
         /// <summary>how much percent current mouth width is comparing to the default.</summary>
+        /// <remarks>This should be range of (0, 150)</remarks>
         public readonly int   MouthWidthPercent;
 
         /// <summary>how much percent current left eye height is comparing to the default.</summary>
+        /// <remarks>This should be range of (0, 150)</remarks>
         public readonly int   LeftEyePercent;
         /// <summary>how much percent current right eye height is comparing to the default.</summary>
+        /// <remarks>This should be range of (0, 150)</remarks>
         public readonly int   RightEyePercent;
     }
 }

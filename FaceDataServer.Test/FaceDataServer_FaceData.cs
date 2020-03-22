3d11b1fbefb33e69bda01e06c4cf621f3f71c6ac
Cj-bc <cj.bc-sd@outlook.jp>
2020-03-22 11:31:00 +0900

    Add: FaceDataComparer to compare FaceDatas

--- FaceDataServer.Test/FaceDataServer_FaceData.cs
+++ FaceDataServer.Test/FaceDataServer_FaceData.cs
@@ -1,6 +1,5 @@
 using Xunit;
 using Cjbc.FaceDataServer.Type;
-using System;
 
 namespace Cjbc.FaceDataServer.UnitTests.Type {
     public class FaceData_FromBinaryShould {
@@ -13,13 +12,47 @@ namespace Cjbc.FaceDataServer.UnitTests.Type {
                                                , 0x00, 0x0a // mouth_height, mouth_width
                                                , 0x64, 0x96 // left_eye, right_eye
                                                };
+        private FaceDataComparer comparer = new FaceDataComparer();
 
         [Fact]
         public void FromBinary_supportedVersionInput_ReturnFaceData() {
             FaceData result = FaceData.FromBinary(testBinary);
 
-            Assert.Equal(result, correctResult);
+            Assert.Equal(correctResult, result, comparer);
         }
+    }
+
+
+
+    /// <summary>
+    ///     Comparer for FaceData.
+    ///     Used by <c>Assert.Equal()</c> to determine 'equality'
+    ///     <see>https://thinkami.hatenablog.com/entry/2015/05/09/071648</see>
+    ///     <see>https://github.com/xunit/assert.xunit/blob/master/EqualityAsserts.cs#L35</see>
+    /// </summary>
+    class FaceDataComparer : System.Collections.Generic.IEqualityComparer<FaceData> {
+        public bool Equals(FaceData a, FaceData b) {
+            return a.FaceXRadian.Equals(b.FaceXRadian)
+                   && a.FaceYRadian.Equals(b.FaceYRadian)
+                   && a.FaceZRadian.Equals(b.FaceZRadian)
+                   && a.MouthHeightPercent.Equals(b.MouthHeightPercent)
+                   && a.MouthWidthPercent.Equals(b.MouthWidthPercent)
+                   && a.LeftEyePercent.Equals(b.LeftEyePercent)
+                   && a.RightEyePercent.Equals(b.RightEyePercent)
+                   ;
+        }
+
+        public int GetHashCode(FaceData a) {
+          return a.FaceXRadian.GetHashCode()
+                 ^ a.FaceYRadian.GetHashCode()
+                 ^ a.FaceZRadian.GetHashCode()
+                 ^ a.MouthHeightPercent.GetHashCode()
+                 ^ a.MouthWidthPercent.GetHashCode()
+                 ^ a.LeftEyePercent.GetHashCode()
+                 ^ a.RightEyePercent.GetHashCode()
+                 ;
+        }
+    
     }
 
 }

5bb2edd730db5564a70852327880c9af9b6b3b26
Cj-bc <cj.bc-sd@outlook.jp>
2020-03-24 17:41:43 +0900

    Add: Unit.ApplyFaceDataToVRM> Unity component to apply FaceData to VRM

---
+++ FaceDataServer/Unity/ApplyFaceDataToVRM.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cjbc.FaceDataServer.Unity;
+using Cjbc.FaceDataServer.Type;
+
+namespace Cjbc.FaceDataServer.Unity {
+
+    /// <summary>
+    ///     Apply FaceData to VRM model
+    ///     Please Attach this component to root object of VRM.
+    ///     <c>FaceDataServerComponent</c> should not be on the same object.
+    /// </summary>
+    public class ApplyFaceDataToVRM : MonoBehaviour
+    {
+        FaceDataServerComponent source;
+        Transform transform;
+        Transform head;
+        FaceData latest;
+        // Start is called before the first frame update
+        void Start()
+        {
+            source = (FaceDataServerComponent)FindObjectOfType(typeof(FaceDataServerComponent));
+            transform = GetComponent<Transform>();
+            head  = transform.Find("Root")
+                        .Find("J_Bip_C_Hips")
+                        .Find("J_Bip_C_Spine")
+                        .Find("J_Bip_C_Chest")
+                        .Find("J_Bip_C_UpperChest")
+                        .Find("J_Bip_C_Neck")
+                        .Find("J_Bip_C_Head")
+                        ;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            latest = source.latest();
+            Quaternion latestRot = Quaternion.Euler( ((float)latest.FaceXRadian) * Mathf.Rad2Deg
+                                                   , ((float)latest.FaceYRadian) * Mathf.Rad2Deg
+                                                   , ((float)latest.FaceZRadian) * Mathf.Rad2Deg
+                                                   );
+            head.localRotation = Quaternion.Lerp(head.localRotation, latestRot, Mathf.Clamp(Time.time * 0.01f, 0.0f, 1.0f));
+        }
+
+
+    }
+}

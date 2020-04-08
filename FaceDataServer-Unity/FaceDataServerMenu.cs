8c84f2e0ad048de6a95242dc258a4b1245a0a777
Cj-bc <cj.bc-sd@outlook.jp>
2020-04-08 17:52:07 +0900

    Add: Unity.FaceDataServerMenu> menu to inject layer& parameters to the controller

---
+++ FaceDataServer-Unity/FaceDataServerMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.Animations;
+using UnityEditor;
+using System.Linq;
+
+namespace Cjbc.FaceDataServer.Unity {
+
+    public class FaceDataServerMenu : MonoBehaviour
+    {
+
+        [MenuItem("FaceDataServer/Inject Required Layer & Parameters")]
+        static void InjectRequiredLayerAndParameter() {
+            
+        }
+
+        [MenuItem("FaceDataServer/Inject Required Layer & Parameters", true)]
+        static bool ValidateInjectRequiredLayerAndParameter() {
+            return Selection.assetGUIDs
+                      .Select(id => AssetDatabase.GUIDToAssetPath(id))
+                      .Select(path => AssetDatabase.GetMainAssetTypeAtPath(path))
+                      .Any(t => t == typeof(AnimatorController));
+        }
+
+    }
+}

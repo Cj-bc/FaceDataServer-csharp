ff81e93e3933fe1c8dd4deb3fa4d112c630efa6b
Cj-bc <cj.bc-sd@outlook.jp>
2020-04-09 11:55:55 +0900

    Fix: ApplyFaceDataToVRM> remove unused import

--- FaceDataServer-Unity/ApplyFaceDataToVRM.cs
+++ FaceDataServer-Unity/ApplyFaceDataToVRM.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 using UnityEditor.Animations;
 using UnityEditor;
 using Cjbc.FaceDataServer.Type;
-using Cjbc.FaceDataServer.Unity.Exceptions;
 using VRM;
 
 namespace Cjbc.FaceDataServer.Unity {
